2e9024bc78b46fae4d52718e47a6ea1b1fcbe10f
Ishan <ishannaik7@gmail.com>
2024-04-07 20:28:07 +0530

    Fixes and added parallax

--- business/src/components/Hero.jsx
+++ business/src/components/Hero.jsx
@@ -1,18 +1,23 @@
 import Section from "./Section";
+import { useRef } from "react";
 import Button from "./Button";
 import curve from "../assets/hero/curve.png";
 import robot from "../assets/hero/robot.jpg"; // Import the robot image
 import heroBackground from "../assets/hero/hero-background.jpg";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
 const Hero = () => {
+  const parallaxRef = useRef();
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25]"
+      className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
             Explore the possibilities of AI Chatting with Brainwave
@@ -40,11 +45,20 @@ const Hero = () => {
             <div className="relative bg-n-8 rounded-[1rem] ">
               <div className="h-[1.4] bg-n-10 rounded-t-[0.9rem] " />
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490]  lg:aspect-[1024/490]">
-                <img src={robot} className="w-full" />
+                <img
+                  src={robot}
+                  className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[33%]"
+                  width={1024}
+                  height={490}
+                  alt="AI"
+                />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex"></ul>
+                </ScrollParallax>
               </div>
             </div>
           </div>
-          <div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
               src={heroBackground}
               className="w-full"
