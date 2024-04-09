1f846db9edc21b382c3ca86a77b3d676f08cb05a
Ishan <ishannaik7@gmail.com>
2024-04-09 09:23:04 +0530

    Updated Footer.jsx

--- business/src/components/Footer.jsx
+++ business/src/components/Footer.jsx
@@ -1,13 +1,15 @@
-import { socials } from "../constants";
 import Section from "./Section";
+import { socials } from "../constants";
+
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
-          ©{new Date().getFullYear()}. All rights reserved.
+          © {new Date().getFullYear()}. All rights reserved.
         </p>
-        <ul className="flex gap-6 flex-wrap">
+
+        <ul className="flex gap-5 flex-wrap">
           {socials.map((item) => (
             <a
               key={item.id}
